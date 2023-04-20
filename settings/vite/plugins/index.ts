9da4ebf9d498c2d9fd04cd71a7fc7bb54ca86482
Leni-microweather <Leni-microweather@proton.me>
2023-04-21 03:35:30 +0800

    config: 配置API按需导入

--- settings/vite/plugins/index.ts
+++ settings/vite/plugins/index.ts
@@ -1,8 +1,10 @@
 import vue from '@vitejs/plugin-vue';
 import { Plugin } from 'vite';
+import { autoImport } from './autoImport';
 
 export const setupPlugins = () => {
     let plugins: Plugin[] = [vue()];
 
+    plugins.push(autoImport()); // 配置API按需导入
     return plugins;
 };
