474a044806eeb75cfce93ac210ba7ab308747dac
Leni-microweather <Leni-microweather@proton.me>
2023-04-21 03:40:41 +0800

    config: 配置自定义组件按需导入

--- settings/vite/plugins/index.ts
+++ settings/vite/plugins/index.ts
@@ -1,10 +1,12 @@
 import vue from '@vitejs/plugin-vue';
 import { Plugin } from 'vite';
 import { autoImport } from './autoImport';
+import { setupComponent } from './components';
 
 export const setupPlugins = () => {
     let plugins: Plugin[] = [vue()];
 
     plugins.push(autoImport()); // 配置API按需导入
+    plugins.push(setupComponent()); // 配置组件按需导入
     return plugins;
 };
