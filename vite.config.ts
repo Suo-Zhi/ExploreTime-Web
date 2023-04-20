388fb089873301454e8a5fe5db0b6b4630d49678
Leni-microweather <Leni-microweather@proton.me>
2023-04-21 03:24:23 +0800

    config: 配置别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
+import { alias } from './settings/vite/alias';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default () => {
+    return {
+        plugins: [vue()],
+        resolve: { alias }, // 设置别名
+    };
+};
