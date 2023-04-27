8148d6d93d635be616ab35caebcee375eff80690
Leni-microweather <Leni-microweather@proton.me>
2023-04-27 23:21:21 +0800

    style: 高度自适应滚动条

---
+++ src/components/base/ScrollBar.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts"></script>
+
+<template>
+    <!-- 滚动条 -->
+    <a-scrollbar class="scroll-box overflow-auto px-2">
+        <slot></slot>
+    </a-scrollbar>
+</template>
+
+<style lang="scss">
+.arco-scrollbar {
+    height: 100%;
+}
+.arco-scrollbar-container {
+    height: calc(100% - 1px);
+}
+.arco-scrollbar-track-direction-vertical {
+    width: 9px !important;
+}
+.arco-scrollbar-thumb-bar {
+    width: 4px !important;
+}
+</style>
