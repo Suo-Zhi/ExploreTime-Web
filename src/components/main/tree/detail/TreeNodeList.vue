1ec5216eb111616551f3e5ca0d6a90f66032db11
Leni-microweather <Leni-microweather@proton.me>
2023-05-07 19:35:47 +0800

    fix(tree): 跨层移动节点无法触发add或remove事件

--- src/components/main/tree/detail/TreeNodeList.vue
+++ src/components/main/tree/detail/TreeNodeList.vue
@@ -12,13 +12,9 @@ const props = withDefaults(defineProps<Props>(), {});
 
 // 改变节点顺序
 const refreshTreeDetail = inject<any>('refreshTreeDetail');
-const changeOrderHandle = async (name: string = '') => {
-    console.log(name);
-    const nodes: Node[] = [];
-    Object.assign(nodes, props.nodes);
-
-    for (let i = 0; i < nodes.length; i++) {
-        const item = nodes[i];
+const changeOrderHandle = async () => {
+    for (let i = 0; i < props.nodes.length; i++) {
+        const item = props.nodes[i];
         await api.treeNode.upsert(item.node?.id || -1, {
             treeId: props.treeId,
             parentNodeId: props.parentNodeId,
@@ -26,31 +22,40 @@ const changeOrderHandle = async (name: string = '') => {
             nodeId: item.id,
         });
     }
-    // 刷新知识树
+    // 更新知识树时间
     await api.tree.updateTime(props.treeId);
-    refreshTreeDetail();
+    // 刷新知识树(setTimeout用来防止刷新太快导致重复触发changeOrderHandle,这方法不是很好,以后改)
+    setTimeout(() => {
+        refreshTreeDetail();
+    }, 200);
 };
 
 // 点击移除
 const deleteHandle = async (index: number) => {
     await api.treeNode.delete(props.nodes[index].node.id).then(async () => {
         props.nodes.splice(index, 1);
-        await changeOrderHandle('del');
     });
 };
+
+// 防止前节点拖拽入后节点内时无法监听到add事件
+watch(
+    () => props.nodes.length,
+    (n, o) => {
+        const diff = n - o;
+        if (diff > 0) changeOrderHandle();
+        else changeOrderHandle();
+    }
+);
 </script>
 
 <template>
     <section class="tree-node-list">
-        {{ props.nodes.length }}
         <drag-list
             :list="props.nodes"
             item-key="node.id"
             group="chunk"
             v-slot="drag"
-            @add="changeOrderHandle('add')"
-            @remove="changeOrderHandle('remove')"
-            @update="changeOrderHandle('update')"
+            @update="changeOrderHandle()"
             class="min-h-[20px]"
         >
             <tree-node
